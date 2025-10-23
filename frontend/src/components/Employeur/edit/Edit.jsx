bf8efa56b1cac578995b81b8680acf2f51ee8233
Raffi Boyajian <2279458@cmontmorency.qc.ca>
2025-10-23 16:24:41 -0400

    Edit (Nom + Email + Enleve client)

--- frontend/src/components/Employeur/edit/Edit.jsx
+++ frontend/src/components/Employeur/edit/Edit.jsx
@@ -1,16 +1,26 @@
 import { useState, useEffect } from "react";
 import { supabase } from "../../../../util/supabaseClient.js";
+import { useParams } from "react-router-dom";
+import "./Edit.css";
 
 const Edit = () => {
-  const [serviceId, setServiceId] = useState(null);
+  const { serviceId } = useParams();
   const [membres, setMembres] = useState([]);
   const getMembres = async () => {
-    const { data, error } = await supabase.from("serviceMembres").select("userId").eq("service_id", serviceId);
+    const { data, error } = await supabase
+      .from("serviceMembres")
+      .select("userId")
+      .eq("service_id", serviceId);
     if (error) {
       console.error("Supabase select error:", error);
       return [];
     }
-    return data.map((record) => record.userId);
+    const userIds = data.map((record) => record.userId);
+    const { data: usersData, error: usersError } = await supabase
+      .from("users")
+      .select("*")
+      .in("id", userIds);
+    setMembres(usersData);
   };
 
   useEffect(() => {
@@ -18,9 +28,42 @@ const Edit = () => {
   }, []);
 
   return (
-    <div>
-      <h1>Edit Job Listing</h1>
-      <p>Select a listing to update or modify its details.</p>
+    <div className="edit-container">
+      <h1>Enlève des clients</h1>
+      <div className="edit-list">
+        {membres.map((membre) => {
+          const handleEnlever = async () => {
+            const confirmDelete = window.confirm(
+              "Es-tu sûr de vouloir effacer ce membre?"
+            );
+
+            if (confirmDelete) {
+              const { error } = await supabase
+                .from("serviceMembres")
+                .delete()
+                .eq("userId", membre.id)
+                .eq("service_id", serviceId);
+
+              if (error) {
+                console.error("Delete failed:", error.message);
+              } else {
+                console.log("Membre effacé avec succès");
+                getMembres();
+              }
+            }
+          };
+
+          return (
+            <div className="edit-card" key={membre.id}>
+              <p>{membre.name}</p>
+              <p>{membre.email}</p>
+              <button className="enleve-bouton" onClick={handleEnlever}>
+                Enlever
+              </button>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
